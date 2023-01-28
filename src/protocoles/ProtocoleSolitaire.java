fb3b601db3effa1e1f15ace03a709e22ef0775f2
Antonia De Oliveira <antonia.deoliveira@laposte.net>
2023-01-28 14:29:44 +0100

    Update ProtocoleSolitaire.java

--- src/protocoles/ProtocoleSolitaire.java
+++ src/protocoles/ProtocoleSolitaire.java
@@ -7,6 +7,7 @@ public class ProtocoleSolitaire  implements Protocole{
     public void executer() {
         //On crée Alice et Bob
         Personne alice = new Personne("Alice");
+        
         Personne bob = new Personne("Bob");
     }
 }
